ac4fa0d201679f87b0844812f407da3c3ae24e35
ebokes <talk2chibu@gmail.com>
2022-07-28 18:48:53 +0100

    feat: fetch data from coingecko API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import Coin from "./components/Coin";
 import "./App.css";
+import { useEffect, useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    Axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false"
+    )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="App">
       <Coin />
