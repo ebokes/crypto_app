1af9a506ed4fe0566b2fcca02e7c56d5ebe010aa
ebokes <talk2chibu@gmail.com>
2022-07-28 18:20:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This react application fetches data through an API and populates the web page.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Coin from "./components/Coin";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Coin />
+    </div>
+  );
+}
+
+export default App;
